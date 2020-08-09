1a6e0021b7d522967a855710b77bcb50a5fa0eee
qidizi <qidizi@qq.com>
2020-08-09 19:08:43 +0800

    加入自定义节目管理功能

--- client/index.js
+++ client/index.js
@@ -4,6 +4,8 @@
         return;
     }
 
+    const MEDIA_KEY = 'media';
+
     window.app = new Vue({
         el: '#app',
         data: {
@@ -15,12 +17,73 @@
             filter: '',
             mask_on: false,
             tv: '',
+            media_list_tmp: [],
+            prompt: false,
+            prompt_ok_show: true,
+            prompt_title: '标题',
+            prompt_data: '',
+            prompt_tip: '请输入',
+            prompt_ok_cb: null,
         },
         watch: {},
         mounted() {
             this.tv = this.get_param('tv', '').trim();
+            this.media_list_tmp_show();
         },
         methods: {
+            prompt_ok() {
+                if (!this.prompt_ok_cb) return;
+                this.prompt_ok_cb();
+            },
+            media_list_tmp_share() {
+                let media = localStorage.getItem(MEDIA_KEY) || '';
+                if (!media) {
+                    this.toast = '暂无自定义视频';
+                    return;
+                }
+
+                this.prompt_ok_show = false;
+                this.prompt = true;
+                this.prompt_title = '请复制以下内容分享给好友';
+                this.prompt_data = media;
+            },
+            media_list_tmp_append() {
+                this.prompt_title = '请输入追加的视频';
+                this.prompt_tip = '视频输入内容格式示例:\n\n片名1 视频地址1\n片名2 视频地址2';
+                this.prompt = true;
+                this.prompt_ok_show = true;
+                let me = this;
+                this.prompt_ok_cb = function () {
+                    let media = (localStorage.getItem(MEDIA_KEY) || '') + "\n" + me.prompt_data;
+                    media = media
+                        .replace(/[ \t\r]+/, ' ')
+                        .replace(/\n+/gm, '\n')
+                        .trim();
+                    localStorage.setItem(MEDIA_KEY, media);
+                    me.media_list_tmp_show();
+                    me.prompt = false;
+                };
+            },
+            media_list_tmp_show() {
+                let media = localStorage.getItem(MEDIA_KEY) || '';
+                let tmp = [];
+                media.split('\n').forEach(function (v, k) {
+                    let m = v.match(/(.+)\s(\w+:\/\/.+)/);
+                    if (!m) return;
+                    tmp.push([m[1], m[2]]);
+                });
+
+                this.media_list_tmp = tmp;
+            },
+            media_list_tmp_del(index) {
+                let tmp = [];
+                this.media_list_tmp.forEach(function (v, k) {
+                    if (index === k) return;
+                    tmp.push(v.join(' '));
+                });
+                localStorage.setItem(MEDIA_KEY, tmp.join('\n'));
+                this.media_list_tmp_show();
+            },
             get_param(name, def) {
                 location.search.replace(
                     new RegExp('[?&]' + name + "=([^&]*)", 'gi'),
